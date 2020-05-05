e41d5cec6c1ad4bb8f32d4f4419434c98e4898de
Myroslav <myroslav.sirenko@tateeda.com>
2020-05-05 23:18:38 +0300

    trying to make modal orders

--- app/js/modals.js
+++ app/js/modals.js
@@ -3,22 +3,51 @@ $('.tooltip').click(function(){
     var buttonId = $(this).attr('id');
     $('#delivery-modal').removeAttr('class').addClass(buttonId);
     $('body').addClass('modal-active');
-  })
-  
-  $('#delivery-modal').click(function(){
-    $(this).addClass('out');
-    $('body').removeClass('modal-active');
-  });
-
-// MODAL ORDER
-$('#btn-order').click(function(){
-    var buttonId = $(this).attr('id');
-    $('#product-request-modal').removeAttr('class').addClass(buttonId);
-    $('body').addClass('modal-active');
 })
-
-$('#product-request-modal').click(function(){
+  
+$('#delivery-modal').click(function(){
     $(this).addClass('out');
     $('body').removeClass('modal-active');
 });
 
+// MODAL ORDER
+$('.card .btn-order').click(function(event){
+    event.stopPropagation();
+    $('#product-request-modal').removeAttr('class').addClass('in');
+    $('body').addClass('modal-active');
+})
+
+$(".modal-background").click(function() {
+    $('.overlay').addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+$('.modal-background').click(function(event){
+    event.stopPropagation();
+});
+
+
+
+
+
+
+
+
+
+
+// $('#product-request-modal').click(function(e){
+//   e.stopPropagation();
+//     $(this).addClass('out');
+//     $('body').removeClass('modal-active');
+// });
+
+// $(document).on('click',function (e) {
+//   var el = '.modal-background';
+//   console.log($(e.target).closest(el));
+//   if ($(e.target).closest(el).length) return;
+//     $('#product-request-modal').addClass('out');
+//     $('body').removeClass('modal-active');
+//  });
+
+
+ 
